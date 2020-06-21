64773b4a7f219cc1f74f5388ba5c9cb58d8362b8
million1998 <1194396343@qq.com>
2020-06-22 00:06:27 +0800

    添加 Nav 样式

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,17 @@
 
 <style lang="scss">
   * {
-    margin: 0;padding: 0;
+    margin: 0;
+    padding: 0;
     box-sizing: border-box;
+  }
+
+  body {
+    line-height: 1.5;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
   }
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;

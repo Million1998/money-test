942d87bf96ef33df6ee057b0ac03394e904b7fb3
million1998 <1194396343@qq.com>
2020-06-21 21:02:58 +0800

    创建<Layout/>

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,17 @@
 <template>
-  <div class="nav-wrapper">
-    <div class="content">
-    Money.vue
-    </div>
-    <Nav/>
+  <div>
+    <Layout>
+    <p>Money.vue</p>
+    </Layout>
   </div>
 </template>
 
 <script lang="ts">
 
+  import Layout from '@/components/Layout.vue';
   export default {
     name: 'Money',
+    components: {Layout},
   };
 </script>
 
-<style lang="scss" scoped>
-    .nav-wrapper{
-      border: 1px solid green;
-      display: flex;
-      flex-direction: column;
-      height: 100vh;
-    }
-  .content{
-    overflow: auto;
-    flex-grow: 1;
-  }
-</style>

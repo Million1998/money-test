f8ce642d02fd0f4a01439f60ae31d2e3518a6a9a
million1998 <1194396343@qq.com>
2021-04-03 23:26:32 +0800

    将tag的string改为{id，name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,11 @@
 const localStorageKeyName = 'tagList';
+type Tag = {
+  id: string;
+  name: string;
+}
 type TagListModel = {
-  data: string[];
-  fetch: () => string[];
+  data: Tag[];
+  fetch: () => Tag[];
   create: (name: string) => string;
   save: () => void;
 }
@@ -9,11 +13,12 @@ const tagListModel: TagListModel = {
   data: [],
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
-    return this.data
+    return this.data;
   },
-  create(name: string){
-    if(this.data.indexOf(name) >=0) { return 'duplicated';}
-    this.data.push(name);
+  create(name: string) {
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) { return 'duplicated';}
+    this.data.push({id: name, name: name});
     this.save();
     return 'success';
   },

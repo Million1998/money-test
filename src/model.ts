9dd91b2b17b270170d167e00cd44a79e3e3e6d09
million1998 <1194396343@qq.com>
2021-04-03 14:15:11 +0800

    封装出一个 model

---
+++ src/model.ts
@@ -0,0 +1,14 @@
+const localStorageKeyName = 'recordList';
+const model = {
+  clone(data: RecordItem[] | RecordItem){
+    return JSON.parse(JSON.stringify(data));
+  },
+  fetch() {
+    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+  },
+  save(date: RecordItem[]) {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(date));
+  }
+};
+
+export default model;
